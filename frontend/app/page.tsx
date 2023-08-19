bc6e0e505cf5023ceff22cbcb3eb6ee71e2fb0a6
tom <83908974+goat6@users.noreply.github.com>
2023-08-19 15:39:39 +0100

    Front end - init & fetching/rendering posts

---
+++ frontend/app/page.tsx
@@ -0,0 +1,34 @@
+const getPosts = async (): Promise<Post[]> => {
+  const res = await fetch("http://127.0.0.1:8080/posts");
+  if (!res.ok) {
+    throw new Error("fetch failed");
+  }
+
+  return res.json();
+};
+
+interface User {
+  id: number;
+  username: string;
+}
+
+interface Post {
+  id: number;
+  title: string;
+  user: User;
+}
+
+export default async function Home() {
+  const posts = await getPosts();
+
+  return (
+    <main>
+      <h1>yonaha-forum</h1>
+      {posts.map((post) => (
+        <>
+          <p>{post.title}</p>
+        </>
+      ))}
+    </main>
+  );
+}
