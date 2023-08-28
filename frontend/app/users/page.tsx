5f8c9e63e1190d8a0856f470dc82c4b9f644a4e2
tom <83908974+goat6@users.noreply.github.com>
2023-08-28 08:38:06 +0100

    fixed fetch by specifying http verb

--- frontend/app/users/page.tsx
+++ frontend/app/users/page.tsx
@@ -1,25 +1,37 @@
 import { User } from "@/interfaces";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const getUsers = async (): Promise<User[]> => {
-  const res = await fetch("http://127.0.0.1:8080/users");
+  const res = await fetch("http://127.0.0.1:8080/users", { method: "GET" });
   if (!res.ok) {
     throw new Error("fetch failed");
   }
-
   return res.json();
 };
 
 const page = async () => {
   const users = await getUsers();
+  // const [users, setUsers] = useState<User[]>([]);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const userData = await getUsers();
+  //       setUsers(userData);
+  //     } catch (error) {
+  //       console.log("error", error);
+  //     }
+  //   };
+  //   fetchData();
+  // }, []);
 
   return (
     <main>
       <h1>Current Users</h1>
       {users.map((user) => (
         <>
-          <p>{user.username}</p>
+          <p key={user.id}>{user.username}</p>
         </>
       ))}
       <Link href={"/"}>To Homepage</Link>
